bd34ce1304fb70bd1b3975d75fc4127a76512bb3
Jayasanka <jayasanka.sack@gmail.com>
2020-01-01 23:51:54 +0530

    Add travis config

---
+++ surge_deploy_test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+npm i -g surge
+# Actual building and setup of current push or PR.
+npm install
+npm run build
+rm -rf node_modules/
+cp build/index.html build/404.html
+
+export SURGE_LOGIN=fossasiasusichat@example.com
+# Token of a dummy account.
+export SURGE_TOKEN=a34fd7119aacd204150edd6a8e29903d
+
+export DEPLOY_DOMAIN=https://kumu-fossasia-susi-web-chat.surge.sh
+surge --project ./build --domain $DEPLOY_DOMAIN;
